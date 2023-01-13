99fbad19a04f5380820775d32a4c75711711cb16
LuoYin <luoyin1986@gmail.com>
2023-01-13 23:17:08 +0800

    update template: add clk and vcd tracing

--- template/main.cpp
+++ template/main.cpp
@@ -1,7 +1,17 @@
 #include "driver.h"
 
 int sc_main(int argc, char *argv[]) {
+  // open vcd file for tracing
+  sc_trace_file *tfile = sc_create_vcd_trace_file("wave");
+
+  // create clock signal
+  sc_clock clk("clk", 10, SC_NS);
+  sc_trace(tfile, clk, "clk");
+
   driver inst_driver("driver");
   sc_start(100, SC_NS);
+
+  // close vcd file
+  sc_close_vcd_trace_file(tfile);
   return 0;
 }
